40940c85dd4b03bcbb3ea3f5fc3dde38bcd895bd
anthify <anth@Anthonys-MacBook-Pro.local>
2018-09-10 15:48:23 +0100

    add html to prettier

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,112 +1,118 @@
-import gulp from 'gulp';
-import gutil from 'gulp-util';
-import babel from 'gulp-babel';
-import concat from 'gulp-concat';
-import uglify from 'gulp-uglify';
-import rename from 'gulp-rename';
-import cleanCSS from 'gulp-clean-css';
-import sass from 'gulp-sass';
-import del from 'del';
-import fileInclude from 'gulp-file-include';
-import imagemin from 'gulp-imagemin';
+import gulp from "gulp";
+import babel from "gulp-babel";
+import concat from "gulp-concat";
+import uglify from "gulp-uglify";
+import rename from "gulp-rename";
+import cleanCSS from "gulp-clean-css";
+import sass from "gulp-sass";
+import del from "del";
+import fileInclude from "gulp-file-include";
+import imagemin from "gulp-imagemin";
 import favicon from "favicons";
 
 import config from "./config";
 
 const paths = {
   styles: {
-    src: 'src/styles/**/*.scss',
-    dest: 'dist/styles/'
+    src: "src/styles/**/*.scss",
+    dest: "dist/styles/"
   },
   scripts: {
-    src: 'src/scripts/**/*.js',
-    dest: 'dist/scripts/'
+    src: "src/scripts/**/*.js",
+    dest: "dist/scripts/"
   },
   html: {
-    src: 'src/routes/*.html',
-    template: 'src/templates/*.html',
-    pages: 'src/pages/*.html',
-    dest: 'dist/'
+    src: "src/routes/*.html",
+    template: "src/templates/*.html",
+    pages: "src/pages/*.html",
+    dest: "dist/"
   },
   images: {
-    src: 'src/images/*',
-    dest: 'dist/images'
+    src: "src/images/*",
+    dest: "dist/images"
   },
   favicon: {
-    src: 'src/favicon/favicon.png',
-    dest: 'dist/' 
+    src: "src/favicon/favicon.png",
+    dest: "dist/"
   }
 };
 
-export const clean = () => del([ 'dist' ]);
+export const clean = () => del(["dist"]);
 
 export function styles() {
-  return gulp.src(paths.styles.src)
-    .pipe(sass().on('error', sass.logError))
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sass().on("error", sass.logError))
     .pipe(cleanCSS())
-    .pipe(rename({
-      basename: 'main',
-      suffix: '.min'
-    }))
+    .pipe(
+      rename({
+        basename: "main",
+        suffix: ".min"
+      })
+    )
     .pipe(gulp.dest(paths.styles.dest));
 }
 
 export function scripts() {
-  return gulp.src(paths.scripts.src, { sourcemaps: true })
+  return gulp
+    .src(paths.scripts.src, { sourcemaps: true })
     .pipe(babel())
     .pipe(uglify())
-    .pipe(concat('main.min.js'))
+    .pipe(concat("main.min.js"))
     .pipe(gulp.dest(paths.scripts.dest));
 }
 
 export function html() {
-  return gulp.src(paths.html.src)
-    .pipe(fileInclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
+  return gulp
+    .src(paths.html.src)
+    .pipe(
+      fileInclude({
+        prefix: "@@",
+        basepath: "@file"
+      })
+    )
     .pipe(gulp.dest(paths.html.dest));
 }
 
 export function images() {
-  return gulp.src(paths.images.src)
-    .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
-      imagemin.svgo({
-        plugins: [
-          {removeViewBox: true},
-          {cleanupIDs: false}
-        ]
-      })
-    ]))
-    .pipe(gulp.dest(paths.images.dest))
+  return gulp
+    .src(paths.images.src)
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }]
+        })
+      ])
+    )
+    .pipe(gulp.dest(paths.images.dest));
 }
 
 export function favicons() {
-  return gulp.src(paths.favicon.src)
-    .pipe(favicon.stream(config))
-    // .on("error", gutil.log)
-    .pipe(gulp.dest(paths.favicon.dest));
+  return (
+    gulp
+      .src(paths.favicon.src)
+      .pipe(favicon.stream(config))
+      // .on("error", gutil.log)
+      .pipe(gulp.dest(paths.favicon.dest))
+  );
 }
 
 export function watch() {
   gulp.watch(paths.styles.src, styles);
   gulp.watch(paths.scripts.src, scripts);
-  gulp.watch([
-    paths.html.pages,
-    paths.html.src,
-    paths.html.template,
-  ],
-  html);
-  gulp.watch([
-    paths.images.src
-  ], images)
+  gulp.watch([paths.html.pages, paths.html.src, paths.html.template], html);
+  gulp.watch([paths.images.src], images);
 }
 
-const build = gulp.series(clean, favicons, gulp.parallel(styles, scripts, html, images));
+const build = gulp.series(
+  clean,
+  favicons,
+  gulp.parallel(styles, scripts, html, images)
+);
 
-gulp.task('build', build);
+gulp.task("build", build);
 
 export default build;
