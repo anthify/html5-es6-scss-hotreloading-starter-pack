e39be2ec1eba5f19a7d82b52250d3acf97ab1bcb
anthify <anthony.thomas.oneill@gmail.com>
2019-07-06 14:29:44 +0100

    dern

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,34 @@
-console.log("👋 from javascript");
+import text from "./text";
+
+const textarea = document.getElementById("textarea");
+textarea.value = text;
+textarea.style.height = `${textarea.scrollHeight}px`;
+
+const editButton = document.querySelector(".c-edit-btn");
+const saveButton = document.querySelector(".c-save-btn");
+const sidebar = document.querySelector(".sidebar-img");
+const sidebarEdit = document.querySelector(".sidebar-img--edit");
+
+editButton.addEventListener("click", () => {
+  textarea.disabled = false;
+  editButton.classList.add("hide");
+  saveButton.classList.remove("hide");
+  sidebar.classList.add("hide");
+  sidebarEdit.classList.remove("hide");
+});
+
+saveButton.addEventListener("click", () => {
+  textarea.disabled = true;
+  editButton.classList.remove("hide");
+  saveButton.classList.add("hide");
+  sidebarEdit.classList.add("hide");
+  sidebar.classList.remove("hide");
+});
+
+sidebarEdit.addEventListener("click", () => {
+  textarea.disabled = true;
+  editButton.classList.remove("hide");
+  saveButton.classList.add("hide");
+  sidebarEdit.classList.add("hide");
+  sidebar.classList.remove("hide");
+});
---
+++ src/scripts/text.js
@@ -0,0 +1,13 @@
+export default `The Insured shall provide to the Insurer a property survey report on the insured addresses such report to be prepared by [Surveyor] ("the Survey").
+
+The Survey shall be so provided by [Time + Date] ("the Survey Deadline").Between inception and the Survey Deadline, cover is provided by the Insurer on the terms and conditions specified in the contract to which this condition is attached ("the Contract Terms").
+
+Where the Survey is not submitted to the Insurer by the Survey Deadline, cover shall terminate at the Survey Deadline.
+
+Where the Survey is submitted to the Insurer by the Survey Deadline, cover shall continue from the Survey Deadline on the Contract Terms until expiry of the period of the contract unless and until terminated in accordance with the following paragraph.
+
+In the event that the Survey is unsatisfactory to the Insurer, the Insurer shall have the right, within 14 days of its receipt, to terminate the contract by serving not less than 14 days' notice in writing to the Insured at its address shown in the contract, such notice expiring no earlier than the Survey Deadline.
+
+In the event of termination under this survey condition, the Insured shall be entitled to pro rata return of premium for the unexpired period of the contract unless a loss has arisen for which the Insured seeks indemnity under this contract in which case the Insurers shall remain entitled to the premium specified in the Contract Terms.
+
+To the extent that this survey condition conflicts with any other cancellation, notice and premium provision in the Contract Terms, this survey condition shall prevail.`;
